fbdd52d5633838cc5e1907389cc3e954e3ca8655
eli-silver <eli_silver@brown.edu>
2025-10-21 23:32:09 -0400

    try adding a new type maybe build pls?

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+declare module 'fft-js' {
+  export function fft(input: number[]): number[][];
+  export function ifft(input: number[][]): number[][];
+  export const util: {
+    fftMag(fftBins: number[][]): number[];
+    fftPhase(fftBins: number[][]): number[];
+  };
+}
